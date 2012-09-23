952e685bfe1491f65c15b32bfd80602f868c7421
teppei machida <machidanohimitsu@gmail.com>
2012-09-23 18:10:12 +0900

    fix training

--- app/controllers/training_controller.rb
+++ app/controllers/training_controller.rb
@@ -2,4 +2,31 @@
 class TrainingController < ApplicationController
   def training1
   end
+  
+  def training2
+  end
+  
+  def training3
+  end
+  
+  def training4
+  end
+  
+  def training5
+  end
+  
+  def training6
+  end
+  
+  def training7
+  end
+  
+  def training8
+  end
+  
+  def training9
+  end
+  
+  def training10
+  end
 end
